ee3c5bdedc34076e74c45d0e50960ea77c5ef0ad
Noura Ous <noura.ousfia@gmail.com>
2022-10-12 16:42:49 +0200

    ajout title_block.html

--- streams/blocks.py
+++ streams/blocks.py
@@ -6,12 +6,11 @@ class TitleBlock(blocks.StructBlock):
     text = blocks.CharBlock(
         required= True,
         help_text= "Text to display"
-
-
     )
+    
 
     class Meta: #pour personnaliser la classe
-        template = "streams/title_block.html"   #fichier HTML qui va representer ce blocks
+        template = "streams/title_block.html"   #fichier HTML qui va representer ce blocks  ????
         icon = "edit"
         label = "Titre"
         help_text = "Texte centré à afficher sur la page"
